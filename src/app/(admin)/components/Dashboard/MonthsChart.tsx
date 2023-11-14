7763d826a6da773c1a4c7dec3e764ea04ecfa2b8
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-11-14 11:17:28 +0300

    dashboard updated

--- src/app/(admin)/components/Dashboard/MonthsChart.tsx
+++ src/app/(admin)/components/Dashboard/MonthsChart.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Title, Legend } from 'chart.js';
-import styles from './monthchart.module.css';
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Title,
+  Legend,
+} from "chart.js";
+import styles from "./monthchart.module.css";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Title, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Title,
+  Legend
+);
 
 const MonthsChart = () => {
   const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
     datasets: [
       {
-        label: 'Orders',
+        label: "Orders",
         data: [12, 19, 15, 21, 9, 16, 10, 14, 20, 25, 9, 13],
-        backgroundColor: 'rgb(41, 218, 192)',
-        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: "rgb(41, 218, 192)",
+        borderColor: "rgba(75, 192, 192, 1)",
         borderWidth: 1,
       },
     ],
--- src/app/(admin)/components/Dashboard/WaveformChart.tsx
+++ src/app/(admin)/components/Dashboard/WaveformChart.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import { Line, Doughnut } from 'react-chartjs-2';
-import styles from './wavefrom.module.css';
+import React from "react";
+import { Line, Doughnut } from "react-chartjs-2";
+import styles from "./wavefrom.module.css";
 
 const WaveformChart = () => {
   const lineChartData = {
-    labels: ['2 May', '13 May', '14 May', '15 May', '16 May', '17 May', '18 May'],
+    labels: [
+      "2 May",
+      "13 May",
+      "14 May",
+      "15 May",
+      "16 May",
+      "17 May",
+      "18 May",
+    ],
     datasets: [
       {
-        label: 'Orders',
+        label: "Orders",
         data: [30, 40, 14, 30, 55, 70, 58],
-        borderColor: 'rgb(39,216,192)',
+        borderColor: "rgb(39,216,192)",
         tension: 0.5,
         borderWidth: 1,
-        borderCapStyle: 'round',
-        borderJoinStyle: 'round',
-        fill: 'start',
-        backgroundColor: 'rgb(39,216,192)',
+        borderCapStyle: "round",
+        borderJoinStyle: "round",
+        fill: "start",
+        backgroundColor: "rgb(39,216,192)",
       },
     ],
   };
@@ -44,11 +52,11 @@ const WaveformChart = () => {
   };
 
   const doughnutChartData = {
-    labels: ['Completed Orders', 'Pending Orders'],
+    labels: ["Completed Orders", "Pending Orders"],
     datasets: [
       {
         data: [25, 10],
-        backgroundColor: ['rgb(39,216,192)', 'rgb(123,129,162)'],
+        backgroundColor: ["rgb(39,216,192)", "rgb(123,129,162)"],
       },
     ],
   };
@@ -56,7 +64,9 @@ const WaveformChart = () => {
   return (
     <div className={styles.chartContainer}>
       <div className={styles.lineChartCard}>
-        <p className= { `${styles.chartTitle} ${styles.salesTitle}` }>Sales From Social Media</p>
+        <p className={`${styles.chartTitle} ${styles.salesTitle}`}>
+          Sales From Social Media
+        </p>
         <Line data={lineChartData as any} options={lineChartOptions} />
       </div>
 
