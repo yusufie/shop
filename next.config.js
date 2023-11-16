3e54c87802b6be4662262ef6d1d0a6b802e27f47
Özkan Demir <ozkanndemir2009@gmail.com>
2023-11-16 19:57:46 +0300

    last fixed

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   images: {
     domains: [
-      'pickbazarlaravel.s3.ap-southeast-1.amazonaws.com',
-      'picsum.photos',
-      'loremflickr.com',
+      "pickbazarlaravel.s3.ap-southeast-1.amazonaws.com",
+      "picsum.photos",
+      "loremflickr.com",
+      "res.cloudinary.com",
     ],
   },
 };
