21c07be5f58799cde92063f6408d8f6fa122733b
Özkan Demir <ozkanndemir2009@gmail.com>
2023-11-16 19:56:03 +0300

    Product fixed

--- src/app/(admin)/components/Modal/Modal.tsx
+++ src/app/(admin)/components/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+"use client"
 import styles from "@/app/(admin)/components/Modal/Modal.module.css";
 import { useState } from "react";
 import { UploadIcon } from "../../assets/icons/UploadIcon";
@@ -6,62 +7,103 @@ interface BasketModalProps {
   onClose: () => void;
 }
 
-const BasketModal: React.FC<BasketModalProps> = ({ onClose }) => {
-  // const [selectedImage, setSelectedImage] = useState<File | null>(null);
 
+const BasketModal: React.FC<BasketModalProps> = ({ onClose }) => {
+
+  // !!!!------------------POST FUNCTİON---------------!!!!
   const [formData, setFormData] = useState({
-    title: '',
-    slug: '',
-    stock: '',
-    description: '',
-    price: '',
-    discount: '',
+    title: "",
+    slug: "",
+    stock: "",
+    description: "",
+    price: "",
+    discount: "",
+    image: null as File | null,
   });
 
-  const handleChange = (e) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+
+    if (
+      name === "image" &&
+      e.target instanceof HTMLInputElement &&
+      e.target.files
+    ) {
+      setFormData({
+        ...formData,
+        [name]: e.target.files[0], 
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const accessToken = localStorage.getItem('accessToken');
+      const accessToken = localStorage.getItem("accessToken");
       if (!accessToken) {
-        throw new Error('Access token not found');
+        throw new Error("Access token not found");
       }
 
-      const response = await fetch(
-        'https://ecommerce-api-5ksa.onrender.com/api/v1/products',
-        {
-          method: 'POST',
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
+      // Step 1: Upload the image
+      const imageFormData = new FormData();
+      if (formData.image) {
+        imageFormData.append("file", formData.image);
+        const imageUploadResponse = await fetch(
+          "https://ecommerce-api-5ksa.onrender.com/api/v1/upload/single",
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+            body: imageFormData,
+          }
+        );
+
+        if (!imageUploadResponse.ok) {
+          throw new Error(
+            `Image upload failed. HTTP error! Status: ${imageUploadResponse.status}`
+          );
         }
-      );
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        const imageUploadData = await imageUploadResponse.json();
+        const imageUrl = imageUploadData.result.url;
+
+        // Step 2: Post product data with the obtained image URL
+        const productResponse = await fetch(
+          "https://ecommerce-api-5ksa.onrender.com/api/v1/products",
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              ...formData,
+              images: [imageUrl], // Use the obtained image URL
+            }),
+          }
+        );
+
+        if (!productResponse.ok) {
+          throw new Error(
+            `Product creation failed. HTTP error! Status: ${productResponse.status}`
+          );
+        }
+
+        const responseData = await productResponse.json();
+        console.log("Post Response:", responseData);
       }
-
-      const responseData = await response.json();
-      console.log('Post Response:', responseData);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
-
-/*   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      setSelectedImage(e.target.files[0]);
-    }
-  }; */
 
   return (
     <>
@@ -75,7 +117,12 @@ const BasketModal: React.FC<BasketModalProps> = ({ onClose }) => {
                 <label htmlFor="fileAdd">
                   <UploadIcon />
                 </label>
-                <input type="file" id="fileAdd" />
+                <input
+                  onChange={handleChange}
+                  type="file"
+                  id="fileAdd"
+                  name="image"
+                />
               </div>
             </div>
           </div>
@@ -85,68 +132,77 @@ const BasketModal: React.FC<BasketModalProps> = ({ onClose }) => {
               Add your Product description and necessary information from here
             </p>
             <form className={styles.formAdd} onSubmit={handleSubmit}>
-            <label htmlFor="title">Name *</label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              value={formData.title}
-              onChange={handleChange}
-              required
-            />
-            <label htmlFor="description">Description</label>
-            <textarea
-              id="description"
-              name="description"
-              value={formData.description}
-              onChange={handleChange}
-            ></textarea>
-            <label htmlFor="slug">Slug</label>
-            <input
-              type="text"
-              id="slug"
-              name="slug"
-              value={formData.slug}
-              onChange={handleChange}
-            />
-            <label htmlFor="price">Price *</label>
-            <input
-              type="number"
-              id="price"
-              name="price"
-              value={formData.price}
-              onChange={handleChange}
-              required
-            />
-            <label htmlFor="stock">Stock</label>
-            <input
-              type="number"
-              id="stock"
-              name="stock"
-              value={formData.stock}
-              onChange={handleChange}
-            />
-            <label htmlFor="discount">Discount</label>
-            <input
-              type="number"
-              id="discount"
-              name="discount"
-              value={formData.discount}
-              onChange={handleChange}
-            />
-            <div className={styles.buttons}>
-              <button type="button" onClick={onClose} className={styles.cancelButton}>
-                Cancel
-              </button>
-              <button type="submit" className={styles.createProductButton}>
-                Create Product
-              </button>
-            </div>
-          </form>
-
+              <label htmlFor="title">Name *</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="description">Description</label>
+              <textarea
+                id="description"
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+              ></textarea>
+              <label htmlFor="slug">Slug</label>
+              <input
+                type="text"
+                id="slug"
+                name="slug"
+                value={formData.slug}
+                onChange={handleChange}
+              />
+              <label htmlFor="price">Price *</label>
+              <input
+                type="number"
+                id="price"
+                name="price"
+                value={formData.price}
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="stock">Stock</label>
+              <input
+                type="number"
+                id="stock"
+                name="stock"
+                value={formData.stock}
+                onChange={handleChange}
+              />
+              <label htmlFor="discount">Discount</label>
+              <input
+                type="number"
+                id="discount"
+                name="discount"
+                value={formData.discount}
+                onChange={handleChange}
+              />
+              {/* <label htmlFor="discount">Categories</label> */}
+              {/* <select name="" id=""> */}
+                {/* <option value="">Category1</option> */}
+                {/* <option value="">Category2</option> */}
+                {/* <option value="">Category3</option> */}
+              {/* </select> */}
+              <div className={styles.buttons}>
+                <button
+                  type="button"
+                  onClick={onClose}
+                  className={styles.cancelButton}
+                >
+                  Cancel
+                </button>
+                <button type="submit" className={styles.createProductButton}>
+                  Create Product
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-{/*         <div className={styles.buttons}>
+        {/*         <div className={styles.buttons}>
           <button onClick={onClose} className={styles.cancelButton}>
             Cancel
           </button>
--- src/app/(admin)/components/Products/Products.tsx
+++ src/app/(admin)/components/Products/Products.tsx
@@ -1,26 +1,141 @@
 "use client"
-import React, { useState } from 'react';
-import styles from '@/app/(admin)/components/Products/Products.module.css';
-import Modal from '@/app/(admin)/components/Modal/Modal';
-import useSWR from 'swr';
-import Image from 'next/image';
+import React, { useState } from "react";
+import styles from "@/app/(admin)/components/Products/Products.module.css";
+import Modal from "@/app/(admin)/components/Modal/Modal";
+import useSWR from "swr";
+import Image from "next/image";
 
-const fetcher = (url:any) => fetch(url).then((res) => res.json());
+const fetchProducts = (url:any) => fetch(url).then((res) => res.json());
 
-const Product = () => {
+const ProductList: React.FC = () => {
   const [selectedOption, setSelectedOption] = useState("option1");
   const [isFocused, setIsFocused] = useState(false);
   const [isModalOpen, setModalOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
-  const { data: datas, error } = useSWR('https://ecommerce-api-5ksa.onrender.com/api/v1/products', fetcher);
+  // !!!----------------------------------GET FUNCTİON--------------------------???
+
+  const {
+    data: datas,
+    error,
+    mutate,
+  } = useSWR(
+    "https://ecommerce-api-5ksa.onrender.com/api/v1/products?limit=20",
+    fetchProducts
+  );
 
   if (error) return <div>Loading failed</div>;
   if (!datas) return <div>Loading...</div>;
   console.log(datas);
+  const fetchData = async () => {
+    const news = await fetchProducts(
+      "https://ecommerce-api-5ksa.onrender.com/api/v1/products?limit=20"
+    );
 
-  const handleSelectChange = (event:any) => {
+    mutate(news);
+  };
+
+  fetchData();
+
+  // !!-----------------------DELETE FUNCTİON-------------------------------------------!!
+  const deleteProduct = async (id: any) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      throw new Error("Access token not found");
+    }
+
+    const response = await fetch(
+      `https://ecommerce-api-5ksa.onrender.com/api/v1/products/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(response);
+    if (!response.ok) {
+      throw new Error("Silme işlemi başarısız oldu.");
+    }
+
+    return response.json();
+  };
+
+  const handleDelete = async (id: any) => {
+    try {
+      await deleteProduct(id);
+      mutate();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const DeleteButton: React.FC<{ id: number }> = ({ id }) => {
+    const handleClick = () => {
+      handleDelete(id);
+    };
+
+    return (
+      <button onClick={handleClick} className={styles.deleteButton}>
+        Sil
+      </button>
+    );
+  };
+  // !!!!!------------------PUT FUNCTİON------------------------------------------!!!!!
+
+  const openUpdateModal = (product: Product) => {
+    setSelectedProduct(product);
+    setModalOpen(true);
+  };
+  const updateProduct = async (id: number, updatedData: Partial<Product>) => {
+    const accessToken = localStorage.getItem("accessToken");
+    
+    if (!accessToken) {
+      throw new Error("Access token not found");
+    }
+
+    const response = await fetch(
+      `https://ecommerce-api-5ksa.onrender.com/api/v1/products/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedData),
+      }
+    );
+
+    const handleUpdate = async () => {
+      try {
+        if (selectedProduct) {
+          const updatedData = {
+            // Güncellenmiş verileri burada belirtin
+            title: "Yeni Başlık",
+            price: 100,
+            // ... diğer alanlar
+          };
+
+          await updateProduct(selectedProduct._id, updatedData);
+          mutate();
+          closeModal(); // veya başka bir işlem yapabilirsiniz
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (!response.ok) {
+      throw new Error("Güncelleme işlemi başarısız oldu.");
+    }
+
+    return response.json();
+  };
+
+  const handleSelectChange = (event: any) => {
     setSelectedOption(event.target.value);
   };
 
@@ -41,7 +156,7 @@ const Product = () => {
   };
 
   const handleSearch = () => {
-    const filteredResults = datas?.data.filter((item:any) =>
+    const filteredResults = datas?.data.filter((item: any) =>
       item?.title.toLowerCase().includes(searchQuery.toLowerCase())
     );
     setSearchResults(filteredResults);
@@ -51,16 +166,18 @@ const Product = () => {
     <>
       <div className={styles.topcontainer}>
         <div className={styles.container}>
-          <div style={{ color: "#161F6A", fontWeight: "bold" }}>
-            Product
-          </div>
-          <div>
-            <select className={styles.select} value={selectedOption} onChange={handleSelectChange}>
-              <option value="option6">Price</option>
-              <option value="option7">Highest to Lowest</option>
-              <option value="option8">Lowest To Highest</option>
-            </select>
-          </div>
+          <div style={{ color: "#161F6A", fontWeight: "bold" }}>Product</div>
+          {/* <div> */}
+            {/* <select */}
+              {/* // className={styles.select} */}
+              {/* // value={selectedOption} */}
+              {/* // onChange={handleSelectChange} */}
+            {/* // > */}
+              {/* <option value="option6">Price</option> */}
+              {/* <option value="option7">Highest to Lowest</option> */}
+              {/* <option value="option8">Lowest To Highest</option> */}
+            {/* </select> */}
+          {/* </div> */}
           <div>
             <input
               type="text"
@@ -68,7 +185,7 @@ const Product = () => {
               className={isFocused ? styles.focused : ""}
               onFocus={handleInputFocus}
               onBlur={handleInputBlur}
-              placeholder='Örnek: İsme Göre Ara'
+              placeholder="Örnek: İsme Göre Ara"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
             />
@@ -77,49 +194,67 @@ const Product = () => {
             </button>
           </div>
           <div> </div>
-          <button onClick={openModal} className={styles.button}>+ Add Product</button>
+          <button onClick={openModal} className={styles.button}>
+            + Add Product
+          </button>
           {isModalOpen && <Modal onClose={closeModal} />}
         </div>
-        <div className={styles.cardcontainer} >
-          {searchResults.length > 0 ? (
-            searchResults.map((item:any) => (
-              <div className={styles.card} key={item._id}>
-                <div className={styles.cardImg}>
-                  <Image src={item.images[0]} alt={item.name} width={256} height={256} />
-                </div>
-                <div className={styles.cardContent}>
-                  <span className={styles.cardPrice}>${item.price}</span>
-                  <p>{item.title}</p>
-                  <div className={styles.afterButton}>
-                    <button >-</button>
-                    <span></span>
-                    <button >+</button>
+        <div className={styles.cardcontainer}>
+          {searchResults.length > 0
+            ? searchResults.map((item: any) => (
+                <div className={styles.card} key={item._id}>
+                  <div className={styles.cardImg}>
+                    <Image
+                      src={item.images[0]}
+                      alt={item.name}
+                      width={256}
+                      height={256}
+                    />
+                  </div>
+                  <div className={styles.cardContent}>
+                    <span className={styles.cardPrice}>${item.price}</span>
+                    <p>{item.title}</p>
+                    <div className={styles.afterButton}>
+                      <DeleteButton id={item._id} />
+                      <button onClick={() => openUpdateModal(item)}>
+                        Güncelle
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
-          ) : (
-            datas?.data.map((item:any) => (
-              <div className={styles.card} key={item._id}>
-                <div className={styles.cardImg}>
-                  <Image src={item.images[0]} alt={item.name} width={256} height={256} />
-                </div>
-                <div className={styles.cardContent}>
-                  <span className={styles.cardPrice}>${item.price}</span>
-                  <p>{item.title}</p>
-                  <div className={styles.afterButton}>
-                    <button >-</button>
-                    <span></span>
-                    <button >+</button>
+              ))
+            : datas?.data.map((item: any) => (
+                <div className={styles.card} key={item._id}>
+                  <div className={styles.cardImg}>
+                    <Image
+                      src={item.images[0]}
+                      alt={item.name}
+                      width={256}
+                      height={256}
+                    />
+                  </div>
+                  <div className={styles.cardContent}>
+                    <span className={styles.cardPrice}>${item.price}</span>
+                    <p>{item.title}</p>
+                    <div className={styles.afterButton}>
+                      <DeleteButton id={item._id} />
+                      <button onClick={() => openUpdateModal(item)}>
+                        Güncelle
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
-          )}
+              ))}
         </div>
+        {/* {selectedProduct && isModalOpen && ( */}
+          {/* // <Modal onClose={() => closeModal()}> */}
+            {/* Güncelleme için gerekli form veya bileşenleri ekleyin */}
+            {/* <button onClick={() => handleUpdate()}>Güncelle</button> */}
+          {/* </Modal> */}
+        {/* // )} */}
       </div>
     </>
   );
-}
+};
 
-export default Product;
+export default ProductList;
