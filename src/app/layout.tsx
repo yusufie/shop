e4016886d21a73a79d2e813d2033ca61be21fe53
yusuf <yusufpirejman@gmail.com>
2023-11-16 21:31:41 +0300

    Update website layout and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'PickBazar',
@@ -16,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body style={{ fontFamily: 'system-ui, sans-serif' }}>
+        {children}
+      </body>
     </html>
   )
 }
