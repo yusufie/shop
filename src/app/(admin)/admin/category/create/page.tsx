8ec4992b2db235f3fa2b2d6775223666f703be11
yusuf <yusufpirejman@gmail.com>
2023-11-19 21:03:09 +0300

    bugfix: admin category create

--- src/app/(admin)/admin/category/create/page.tsx
+++ src/app/(admin)/admin/category/create/page.tsx
@@ -1,33 +1,11 @@
 import Layout from "@/app/(admin)/components/Layout/Layout"
 import CategoryForm from "@/app/(admin)/components/Forms/Category/CategoryForm"
-
-import { revalidateTag } from "next/cache";
-
-async function getCategories() {
-  // Cache data and tag it for revalidation
-  const res = await fetch(
-    "https://ecommerce-api-5ksa.onrender.com/api/v1/categories",
-    {
-      next: { tags: ["categoriesData"] },
-    }
-  );
-
-  if (res.ok) {
-    await revalidateTag("categoriesData"); // Revalidate the tagged cache entry
-  }
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
+import getCategories from "@/utils/getCategories";
 
 async function CreatePage() {
 
   const categoriesData = await getCategories();
-  // console.log(categoriesData)
+  // console.log("categoriesData:", categoriesData)
 
   return (
     <Layout>
