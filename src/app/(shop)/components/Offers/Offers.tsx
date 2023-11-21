9b443cd02a8f1b0369a514ee8916ed78dd550275
yusuf <yusufpirejman@gmail.com>
2023-11-21 14:43:00 +0300

    refactor: cupon card styling in offers component

--- src/app/(shop)/components/Offers/Offers.tsx
+++ src/app/(shop)/components/Offers/Offers.tsx
@@ -8,13 +8,13 @@ const Offers = () => {
       <div className={styles.cupons}>
         {cupons.map((cupon) => (
           <div className={styles.cuponCard} key={cupon._id}>
-            <div>
+            <div className={styles.cuponHeader}>
               <Image src={cupon.image} alt="cupon" width={270} height={200} />
             </div>
 
             <div className={styles.cuponInfo}>
-              <h4>{cupon.code}</h4>
-              <button>Copy</button>
+              <h4 className={styles.cuponCode}>{cupon.code}</h4>
+              <button className={styles.cuponButton}>Copy</button>
             </div>
           </div>
         ))}
