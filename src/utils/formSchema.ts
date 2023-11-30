998912bf3001221e336854b27ff0eed2419d189e
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-11-30 05:10:17 +0300

    teset password schema added

--- src/utils/formSchema.ts
+++ src/utils/formSchema.ts
@@ -17,3 +17,8 @@ export const registerSchema = z.object({
 export const forgotPasswordSchema = z.object({
   email: z.string().email({ message: 'Invalid email address' }),
 });
+
+export const resetPasswordSchema = z.object({
+  password: z.string().min(8, { message: 'Password must be at least 8 characters' }),
+  passwordConfirm: z.string().min(8, { message: 'Password must be at least 8 characters'}),
+});
