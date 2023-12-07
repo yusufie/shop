5a8ffad758721e2630810f53f0ed5214f378c405
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-12-07 17:34:52 +0300

    products page fixed

--- src/app/(shop)/products/[id]/page.tsx
+++ src/app/(shop)/products/[id]/page.tsx
@@ -2,20 +2,21 @@ import Layout from "@/app/(shop)/components/Layout/Layout";
 import ProductDetails from "@/app/(shop)/components/Details/Product/ProductDetails";
 import Bag from "@/app/(shop)/components/Bag/Bag";
 import getCategories from "@/utils/getCategories";
-import { revalidateTag } from "next/cache";
+import getProducts from "@/utils/getProducts";
+// import { revalidateTag } from "next/cache";
 
-async function getProducts() {
-  // Cache data and tag it for revalidation
-  const res = await fetch( "https://ecommerce-api-5ksa.onrender.com/api/v1/products",
-    { next: { tags: ["productData"] }, }
-  );
+// async function getProducts() {
+//   // Cache data and tag it for revalidation
+//   const res = await fetch( "https://ecommerce-api-5ksa.onrender.com/api/v1/products",
+//     { next: { tags: ["productData"] }, }
+//   );
 
-  if (res.ok) { await revalidateTag("productData"); }
+//   if (res.ok) { await revalidateTag("productData"); }
 
-  if (!res.ok) { throw new Error("Failed to fetch data"); }
+//   if (!res.ok) { throw new Error("Failed to fetch data"); }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 async function DynamicPage() {
 
