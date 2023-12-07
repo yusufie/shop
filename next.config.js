3a8d248275750fc00a4cd8516475fb1f94dc367b
yusuf <yusufpirejman@gmail.com>
2023-12-07 21:52:05 +0300

    Update next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/shop",
-  assetPrefix: `https://yusufie.github.io/shop/`,
   images: {
     domains: [
       "pickbazarlaravel.s3.ap-southeast-1.amazonaws.com",
