31bf5288316fb3ed7b636f3d99bf193c923c5e3c
Özkan Demir <ozkanndemir2009@gmail.com>
2023-12-15 02:25:05 +0300

    Last fixed and function added

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,6 +1,9 @@
 import Layout from "@/app/(shop)/components/Layout/Layout"
 import Checkout from "@/app/(shop)/components/Checkout/Checkout"
 
+//make page dynamic force in nextjs
+export const dynamic = 'force-dynamic'
+
 function CheckoutPage() {
 
   return (
